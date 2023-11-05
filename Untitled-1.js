09f35be114be3e8bfc8382fc35decd840070339f
1986allex <1986allex@gmail.com>
2023-11-05 13:17:24 +0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -4,3 +4,12 @@ const bot = new Telegraf("6082306857:AAF_vGCBs91VLz6vZh0RLxqBwHt837wpZOo");
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send("hello")
+}
+app.listen(port, () => {
+})
