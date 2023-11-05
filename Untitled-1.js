e2061eedd983a60095ab34708ac495a2223e1c1c
1986allex <1986allex@gmail.com>
2023-11-05 13:09:21 +0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf("6082306857:AAF_vGCBs91VLz6vZh0RLxqBwHt837wpZOo");
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
