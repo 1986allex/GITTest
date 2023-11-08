9f9c90edd5c0cee27ab693d96c651b3cc81efefd
1986allex <1986allex@gmail.com>
2023-11-08 21:59:43 +0300

    111

--- Untitled-1.js
+++ Untitled-1.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send("hello")
+  res.send(<div onclick="alert()">sddfsdf</div>)
 })
 app.listen(port, () => {
 })
