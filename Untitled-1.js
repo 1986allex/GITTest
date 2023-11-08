0a8814d18a3a50d2c00c7c0a34c2ee6b52f7aca9
1986allex <1986allex@gmail.com>
2023-11-08 22:13:47 +0300

    111

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,15 +1,19 @@
 const { Telegraf } = require('telegraf');
-
+let msg;
 const bot = new Telegraf("6082306857:AAF_vGCBs91VLz6vZh0RLxqBwHt837wpZOo");
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.use(session())
+bot.hears('hi', (ctx) => {
+    msg = ctx.message;
+    ctx.reply('Hey there')
+});
 bot.launch();
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('<div onclick="alert()">sddfsdf</div>')
+  res.send('<div onclick="alert(' + msg + ')">sddfsdf</div>')
 })
 app.listen(port, () => {
 })
